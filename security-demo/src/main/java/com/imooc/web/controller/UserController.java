bf4617f04f9e9ee0a627c79f66efa2170321a160
kafun18 <513112374@qq.com>
2019-10-14 22:40:03 +0800

    securitydemo

---
+++ security-demo/src/main/java/com/imooc/web/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.imooc.web.controller;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+	@RequestMapping
+	public List<User> query(){
+		
+	}
+}
